ca9ed059f5695dcddea4b704380ea6d5674c6ac0
Patrick Amadeus Irawan <patrickai0309@gmail.com>
2021-11-02 00:02:57 +0700

    Finishing FE & partial BE configuration

--- src/web/index.js
+++ src/web/index.js
@@ -43,12 +43,12 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   function updateThumbnail(dropZoneElement, file) {
     let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
   
-    // First time - remove the prompt
+    // Removing Prompt
     if (dropZoneElement.querySelector(".drop-zone__prompt")) {
       dropZoneElement.querySelector(".drop-zone__prompt").remove();
     }
   
-    // First time - there is no thumbnail element, so lets create it
+    // Kondisi tidak ada Thumbnail Element, pembuatan baru
     if (!thumbnailElement) {
       thumbnailElement = document.createElement("div");
       thumbnailElement.classList.add("drop-zone__thumb");
@@ -69,81 +69,55 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
       thumbnailElement.style.backgroundImage = null;
     }
   }
+
   
   const formImage = document.getElementById("reportImage");
   formImage.onsubmit = async function (event) {
+
     event.preventDefault();
     const inputFiles = document.getElementById("reportFile");
+    const inputCompression = document.getElementById("reportCompression");
+    const percentageShow = document.getElementById("perc");
+
+    console.log(inputCompression.value);
+    // conditional situation untuk tipe file yang di-upload
+    if (inputFiles.files[0] !== undefined && inputFiles.files[0].type.startsWith("image/")){
+      sessionStorage.clear();
+
+      percentageShow.innerHTML = inputCompression.value + "% Compression";
   
-    let form = new FormData();
-    form.append("detail", "1");
-    form.append("file", inputFiles.files[0]);
-  
-    try {
-      let response = await fetch("https://api.detax.org/detax/v1/scan_image", {
-        method: "POST",
-        body: form,
-      });
-      response = await response.json();
-  
-      const body = {
-        classification: response.result.scan_result,
-        description: "",
-        image_url: "",
-        refs: [],
-        report_id: response.result.report_id,
-      };
-  
-      // Kalau hoax, kirim report
-      if (body.classification !== 1) {
-        response = await fetch("https://api.detax.org/detax/v1/report/image", {
-          method: "POST",
-          body: JSON.stringify(body),
-        });
-        response = await response.json();
-        console.log(response);
-      } else {
-        console.log("Ini fakta");
-      }
-    } catch (error) {
-      console.log(error);
+      console.log("VALID");
+      const reader = new FileReader();
+      reader.readAsDataURL(inputFiles.files[0]);
+
+      var beforeImg = document.getElementById('before-image');
+      var afterImg = document.getElementById('after-image');
+
+      beforeImg.src = URL.createObjectURL(inputFiles.files[0]);
+      afterImg.src = URL.createObjectURL(inputFiles.files[0]);
+
+      reader.addEventListener("load",() => {
+        sessionStorage.setItem("image" , reader.result);
+      })
+    }
+    else{
+      percentageShow.innerHTML = "-";
+      console.log("NO");
     }
   };
+
+
+  const targetDiv = document.getElementById("compression-result");
+  const submit = document.getElementById("show-button");
+
+  submit.onclick = function () {
+    if (targetDiv.style.display !== "block") {
+      targetDiv.style.display = "block";
+    }
+    else{
+      targetDiv.style.display = "none";
+    }
+  };
+
   
-  // const formText = document.getElementById("reportText");
-  // const formTextContent = document.getElementById("formReportText");
-  // formText.onsubmit = async function (event) {
-  //   event.preventDefault();
-  //   const body = JSON.stringify({
-  //     text: formTextContent.value,
-  //     detail: 1,
-  //   });
-  
-  //   try {
-  //     let response = await fetch("https://api.detax.org/detax/v1/scan_text", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: body,
-  //     });
-  //     response = await response.json();
-  
-  //     if (response.result.scan_result !== 1) {
-  //       response = await fetch("https://api.detax.org/detax/v1/report/text", {
-  //         method: "POST",
-  //         body: JSON.stringify({
-  //           text: formTextContent.value,
-  //           classification: response.result.scan_result,
-  //           description: "",
-  //           refs: [],
-  //         }),
-  //       });
-  //       response = await response.json();
-  //       console.log(response);
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
   
