83458727a2761cb1e6c1801536f7b8911da3e624
raniadf <16520006@mahasiswa.itb.ac.id>
2021-11-09 22:05:52 +0700

    implement qr iteration to find eigenvalues

--- src/compress/svdeigen.py
+++ src/compress/svdeigen.py
@@ -1,35 +1,71 @@
 import numpy as np
 from numpy.matrixlib.defmatrix import matrix # buat operasi matriks
-import sympy as sym # buat bikin matriks yang pake lamda
-import random #buat gauss function
-# atau from sympy import * ???
-# INI BLM DICOBA RUN SAMSEK KARENA SYMNYA ERROR
+import math as mp
+from random import gauss
 
-#KALAU BISA HASILNYA DALAM BENTUK ARRAY
-def getEigenValues(matriks):
-    row = len(matriks)
-    col = len(matriks[0])
+def getEigenValues(m):
+    row = len(m)
+    col = len(m[0])
+    eigval = []
 
-    # 1. Bikin matriks lambda
-    # bisa juga langsung np.identity(row)
-    lamda = sym.symbols('lamda')
-    matId = sym.eye(row)
-    for i in range (row) :
-        for j in range (col) :
-            if (j == i):
-                #matId[i][j] = lamda
+    # 1. Membuat atribut class untuk elemen matriks
+    class C:
+        # Fungsi untuk assign nilai dari elemen
+        def __init__(self, i, j, tan = 0):
+            self.i = i
+            self.j = j
+            self.sin = tan / mp.sqrt(1.0 + tan * tan) 
+            self.cos = 1.0 / mp.sqrt(1.0 + tan * tan)
+
+    # 2. Melakukan iterasi QR
+    # range = jumlah iterasi
+    for i in range(100):
+    # a. mengimplementasikan iterasi QR --> (A(k+1) = Q^T x A(k) x Q)
+    # menginisialisasi list
+        newList = []
+    # melakukan operasi perkalian dari kiri, menghapus 0 dari kolom j
+        for j in range(col):
+            for i in range(row - 1, j, -1):
+                if abs(m[j][j]) > 1e-8 :
+                    tan = m[i][j] / m[j][j]
+                    newlist_1 = C(i, j, tan)
+                else:
+                    s = C(i, j)
+                    s.sin = 1.0
+                    s.cos = 0.0
+                    newlist_1 = s
+
+                for i in range(col):
+                    temp = m[newlist_1.j][i] * newlist_1.cos + m[newlist_1.i][i] * newlist_1.sin
+                    m[newlist_1.i][i] = m[newlist_1.i][i] * newlist_1.cos - m[newlist_1.j][i] * newlist_1.sin
+                    m[newlist_1.j][i] = temp
+                newList.append(newlist_1)
+        
+        for el in newList:
+            for i in range(row):
+                temp = m[i][el.j] * el.cos + m[i][el.i] * el.sin
+                m[i][el.i] = m[i][el.i] * el.cos - m[i][el.j] * el.sin
+                m[i][el.j] = temp
     
-    # ini kalo matriks identitas doang blm di lamdain
-    #matId = np.zeros((row,col))
-    for i in range (row) :
-        for j in range (col) :
-            if (j == i):
-                matId[i][j] = 1
-    
-    # 2. Mencari fungsi determinan det(lamda.I - A)
+    # 3. Mengambil matriks yang berada pada diagonal utama (nilai eigen dari matriks)
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            if (i == j):
+                eigval.append(m[i][j])
 
-    # 3. Mencari persamaan karakteristik -> det = 0
+    # 4. Mengurutkan nilai eigen yang telah didapatkan untuk mempermudah proses perhitungan pada fungsi-fungsi lainnya    
+    for i in range(len(eigval)):
+        max = eigval[i]
+        j = i + 1
+        while j < len(eigval) :
+            if eigval[j] > max :
+                max = eigval[j]
+                eigval[j] = eigval[i]
+                eigval[i] = max
+            j += 1
 
+    return eigval
+            
 #INI DIPAKENYA NANTI LOOPING SEMUA EIGEN VALUE
 def getEigenVector(matriks, eigval):
     # 1. (lamda.I - A)X = 0 
@@ -39,7 +75,7 @@ def getEigenVector(matriks, eigval):
 
     # 3. Bikin matriks dari hasil persamaan karakteristiknya
 
-def getsvd(matriks):
+#def getSVD(matriks):
     ## A. SINGULAR KIRI
     # 1. kali matriks dengan matriks transposenya -> A.A^T
     m = matriks
@@ -87,3 +123,20 @@ def getsvd(matriks):
 
     # 11. A = U x E x V^T
     # return U, E, V^T
+
+
+### TEST CASE UNTUK EIGENVALUES
+test = [1.0, -1.0, 0.0, 0.0, -1.0, 1.0, -1.0, 0.0, 0.0, -1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 3.0] # Example from the book
+#test = [1.0 , 3.0, 3.0, 1.0]
+m = np.array(test).reshape((4, 4))
+
+print("\n --- Built-in ---")
+print(np.linalg.eig(m)[0]) # uses _geev LAPACK routines (see references)
+
+print("\n --- QR ITERATION ---")
+print(getEigenValues(m))
+
+### TEST CASE UNTUK EIGENVECTOR
+
+
+### TEST CASE UNTUK SVD
